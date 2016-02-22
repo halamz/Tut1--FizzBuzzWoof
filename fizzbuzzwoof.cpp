baa1d36bb90aabb63a73b0fce0dc21fa9d3e149e
halamz <214519970@stu.ukzn.ac.za>
2016-02-22 11:23:02 +0200

    part1 counts form 1 to 20 and check s if number is divisable by 3,5,both or neighter

---
+++ fizzbuzzwoof.cpp
@@ -0,0 +1,30 @@
+//part 1: counts from 1 to 20 and checks if number is divisable by 3 and/or 5 and/or 7 or neighter.
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+	string comment = "";
+	for (int i = 1; i <= 20; i++)
+	{
+		comment = "";
+		if (i % 3 == 0)
+		{
+			comment = "Fizz ";
+		}
+		if (i % 5 == 0)
+		{
+			comment = comment + "Buzz ";
+		}
+
+		if (comment == "")
+		{
+			cout << i << endl;
+		}
+		else
+		{
+			cout << comment << endl;
+		}
+	}
+}
